84c59e4bfa3ab7ecc5ca4ab8819ee4b940c3b734
Owl <gidra4367@protonmail.com>
2023-07-26 23:26:58 +0300

    Without ball with errors :)

---
+++ src/main.rs
@@ -0,0 +1,161 @@
+use ggez::{ Context, ContextBuilder, GameResult };
+use ggez::graphics::{ Color, Canvas, Rect, DrawMode, Mesh, FillOptions, DrawParam, InstanceArray, Image };
+use ggez::event::{ EventHandler };
+use ggez::event;
+use ggez::conf::WindowMode;
+use ggez::input::keyboard::KeyCode;
+
+enum Side {
+    Left,
+    Right,
+    Up,
+    Down
+}
+
+struct Player {
+    x1: f32,
+    y1: f32,
+    width: f32,
+    height: f32
+}
+
+impl Player {
+    pub fn new(side: Side, size_window: (f32, f32)) -> Result<Self, ()> {
+        let w: f32 = 20.0;
+        let h: f32 = 100.0;
+        let x: f32;
+        let y: f32 = size_window.1 / 2. - h / 2.;
+
+        match side {
+            Side::Left => {
+                x = 0.0;
+            }
+
+            Side::Right => {
+                x = size_window.0 - w;
+            }
+
+            _ => { return Err(()); } 
+        }
+
+        Ok(Player { x1 : x, y1 : y, height : h, width : w })
+    }
+
+    fn move_up(&mut self, speed: f32) {
+        if self.y1 > speed { 
+            self.y1 -= speed; 
+        } else { self.y1 = 0.; }
+    }
+
+    fn move_down(&mut self, speed: f32, window_y: f32) {
+        if  self.y1 + 
+            self.height + 
+            speed
+            > window_y { 
+                self.y1 = window_y - self.height;
+        } else { self.y1 += speed; }
+    }
+
+    pub fn y_move(&mut self, side: Side, speed: f32, window_y: f32) {
+        match side {
+            Side::Up => self.move_up(speed),
+            Side::Down => self.move_down(speed, window_y),
+            _ => todo!()
+        }
+    }
+}
+
+struct MyGame {
+    player1: Player,
+    player2: Player
+}
+
+impl MyGame {
+    pub fn new(_ctx: &mut Context) -> Self {
+        // Load/create resources such as images here.
+        let window_mode = WindowMode::default();
+        let window_size = (window_mode.width, window_mode.height);
+
+        MyGame {
+            player1: Player::new(Side::Left, window_size).unwrap(),
+            player2: Player::new(Side::Right, window_size).unwrap()
+        }
+    }
+}
+
+impl EventHandler for MyGame {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        let speed_players = 4.;
+
+        let (window_x, window_y) = (
+            WindowMode::default().width,
+            WindowMode::default().height
+        );
+
+        let move_keys = Vec::from([KeyCode::W, KeyCode::S, KeyCode::Up, KeyCode::Down]);
+        for i in 0..move_keys.len() {
+            if _ctx.keyboard.is_key_pressed(move_keys[i]) {
+                match i {
+                    0 => self.player1.y_move(Side::Up, speed_players, window_y),
+                    1 => self.player1.y_move(Side::Down, speed_players, window_y),
+                    2 => self.player2.y_move(Side::Up, speed_players, window_y),
+                    3 => self.player2.y_move(Side::Down, speed_players, window_y),
+                    _ => todo!()
+                }
+            }
+        }
+        
+        
+        
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        let mut canvas = Canvas::from_frame(ctx, Color::BLACK);
+
+        let player1 = Mesh::new_rectangle(
+            ctx, 
+            DrawMode::Fill(FillOptions::DEFAULT), 
+            Rect { 
+                x : self.player1.x1, y : self.player1.y1, 
+                w : self.player1.width, h : self.player1.height 
+            }, 
+            Color::WHITE
+        ).unwrap();
+        let player2 = Mesh::new_rectangle(
+            ctx, 
+            DrawMode::Fill(FillOptions::DEFAULT), 
+            Rect { 
+                x : self.player2.x1, y : self.player2.y1, 
+                w : self.player2.width, h : self.player2.height 
+            }, 
+            Color::WHITE
+        ).unwrap();
+
+        canvas.draw(
+            &player1,
+            DrawParam::new()
+        );
+        canvas.draw(
+            &player2,
+            DrawParam::new()
+        );
+
+        canvas.finish(ctx)
+    }
+}
+
+fn main() {
+    // Make a Context.
+    let (mut ctx, event_loop) = ContextBuilder::new("Pong", "By God")
+        .build()
+        .expect("aieee, could not create ggez context!");
+
+    // Create an instance of your event handler.
+    // Usually, you should provide it with the Context object to
+    // use when setting your game up.
+    let my_game = MyGame::new(&mut ctx);
+
+    // Run!
+    event::run(ctx, event_loop, my_game);
+}
